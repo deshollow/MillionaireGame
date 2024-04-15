2ae29c794952333e274e33a13b6f41cd3a589f6f
deshollow <555demin@gmail.com>
2024-04-15 15:46:18 +0300

    Initial Commit

---
+++ MillionaireGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MillionaireGame
+//
+//  Created by deshollow on 15.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
